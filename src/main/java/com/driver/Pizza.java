4fc5cc013cfedb178fb8976dec15f04cff295c7c
Raju-ranjan-nitp <raju.ranjan14@gmail.com>
2023-06-20 11:25:59 +0530

    pizza bill assignment

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,26 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private Boolean containCheese;
+    private Boolean containToppings;
+    private Boolean isBag;
+
+
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg){
+            price=300;
+        }else{
+            price = 400;
+        }
+        containCheese=false;
+        containToppings= false;
+        isBag=false;
+
+        this.bill="";
+        setBill("Base Price Of The Pizza: "+ price +"\n");
     }
 
     public int getPrice(){
@@ -17,18 +34,53 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!containCheese){
+            price+=80;
+            this.containCheese=true;
+            setBill("Extra Cheese Added: "+80+"\n");
+        }
+
+    }
+
+    public void setisVeg(Boolean isVeg) {
+        this.isVeg = isVeg;
+    }
+
+    public Boolean getisVeg() {
+        return isVeg;
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!containToppings){
+            if(isVeg){
+                price+=70;
+                setBill("Extra Toppings Added: 70\n");
+                this.containToppings=true;
+            }else{
+                price+=120;
+                setBill("Extra Toppings Added: 120\n");
+                this.containToppings=true;
+            }
+        }
+
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!isBag){
+            this.price+=20;
+            isBag=true;
+            setBill("Paperbag Added: 20\n");
+        }
+    }
+
+    public void setBill(String bill) {
+        this.bill+= bill;
     }
 
     public String getBill(){
-        // your code goes here
-        return this.bill;
+        // your code goes her
+        return this.bill+"Total Price: "+this.price+"\n";
     }
 }
